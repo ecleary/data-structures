7d723931334038fa809e99604bcf2d3b7217901c
Errol Cleary <errol.cleary@gmail.com>
2020-11-27 15:43:25 -0800

    Add time complexity notes for Tree methods

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -8,11 +8,13 @@ var Tree = function(value) {
 
 var treeMethods = {};
 
+// Time complexity: O(1)
 treeMethods.addChild = function(value) {
   var newTree = Tree(value);
   this.children.push(newTree);
   return newTree;
 };
+// Time complexity: O(n)
 treeMethods.contains = function(value) {
   if (this.value === value) {
     return true;
