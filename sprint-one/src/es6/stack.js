3a5346ef6eef8949cb28a61b3af93aa6e4b8e32d
Errol Cleary <errol.cleary@gmail.com>
2020-11-27 11:41:45 -0800

    Implement Stack in pseudoclassical and ES6 instantiation styles

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,24 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // Instantiated and returned object and properties
   constructor() {
+    this.storage = {};
   }
-
+  // Methods on prototype chain
+  push(string) {
+    var nextKey = this.size();
+    this.storage[nextKey] = string;
+    return this.size();
+  }
+  pop() {
+    var topKey = this.size() - 1;
+    if (topKey > -1) {
+      var removedValue = this.storage[topKey];
+      delete this.storage[topKey];
+      return removedValue;
+    }
+    return -1;
+  }
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+Stack.prototype.push = function(string) {
+  var nextKey = this.size();
+  this.storage[nextKey] = string;
+  return this.size();
+};
 
+Stack.prototype.pop = function() {
+  var topKey = this.size() - 1;
+  if (topKey > -1) {
+    var removedValue = this.storage[topKey];
+    delete this.storage[topKey];
+    return removedValue;
+  }
+  return -1;
+};
+
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
