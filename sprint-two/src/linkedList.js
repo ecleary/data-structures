92a48cb7c5839e7ffdd14b0779c84c9757787d43
Errol Cleary <errol.cleary@gmail.com>
2020-11-27 15:23:44 -0800

    Implement LinkedList in functional instantiation style

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,29 +1,50 @@
 var LinkedList = function() {
-  var list = {};
-  list.head = null;
-  list.tail = null;
-
-  list.addToTail = function(value) {
+  var someInstance = {};
+  someInstance.head = null;
+  someInstance.tail = null;
+  someInstance.addToTail = function(value) {
+    var newNode = Node(value);
+    if (someInstance.head === null) {
+      someInstance.head = newNode;
+    }
+    if (someInstance.tail !== null) {
+      someInstance.tail.next = newNode;
+    }
+    someInstance.tail = newNode;
+    return newNode;
   };
-
-  list.removeHead = function() {
+  someInstance.removeHead = function() {
+    if (someInstance.head !== null) {
+      var removedHead = someInstance.head.value;
+      if (someInstance.head.next !== null) {
+        someInstance.head = someInstance.head.next;
+      } else {
+        someInstance.head = null;
+      }
+      return removedHead;
+    } else {
+      return -1;
+    }
   };
-
-  list.contains = function(target) {
+  someInstance.contains = function(value) {
+    var nodeToCheck = someInstance.head;
+    while (true) {
+      if (nodeToCheck.value === value) {
+        return true;
+      }
+      if (nodeToCheck.next !== null) {
+        nodeToCheck = nodeToCheck.next;
+      } else {
+        return false;
+      }
+    }
   };
-
-  return list;
+  return someInstance;
 };
 
 var Node = function(value) {
   var node = {};
-
   node.value = value;
   node.next = null;
-
   return node;
 };
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
