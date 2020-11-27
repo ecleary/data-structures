ece183ee26dc8850614948f7c3b28730ca30361a
Errol Cleary <errol.cleary@gmail.com>
2020-11-27 13:38:49 -0800

    Implement Stack in functional, functional-shared, and prototypal instantiation styles

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  push: function(value) {
+    var nextKey = this.size();
+    this.storage[nextKey] = value;
+    return this.size();
+  },
+  pop: function() {
+    var topKey = this.size() - 1;
+    if (topKey > -1) {
+      var removedValue = this.storage[topKey];
+      delete this.storage[topKey];
+      return removedValue;
+    }
+    return -1;
+  },
+  size: function() {
+    return Object.keys(this.storage).length;
+  }
+};
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,22 @@
 var Stack = function() {
   var someInstance = {};
-
-  // Use an object with numeric keys to store values
-  var storage = {};
-
-  // Implement the methods below
+  someInstance.storage = {};
   someInstance.push = function(value) {
+    var nextKey = someInstance.size();
+    someInstance.storage[nextKey] = value;
+    return someInstance.size();
   };
-
   someInstance.pop = function() {
+    var topKey = someInstance.size() - 1;
+    if (topKey > -1) {
+      var removedValue = someInstance.storage[topKey];
+      delete someInstance.storage[topKey];
+      return removedValue;
+    }
+    return -1;
   };
-
   someInstance.size = function() {
+    return Object.keys(someInstance.storage).length;
   };
-
   return someInstance;
 };
