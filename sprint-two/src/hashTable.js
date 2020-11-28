ceb9017a5a43a75e56bf30fc7eb5f40c735a7429
Errol Cleary <errol.cleary@gmail.com>
2020-11-28 11:06:27 -0800

    Implement HashTable in pseudoclassical instantiation style

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,26 +1,66 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.insert = function(key, value) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, []);
+  }
+  var bucket = this._storage.get(index);
+  var existingSlot = -1;
+  for (var slot = 0; slot < bucket.length; slot++) {
+    if (bucket[slot][0] === key) {
+      existingSlot = slot;
+      break;
+    }
+  }
+  if (existingSlot > -1) {
+    bucket[existingSlot] = [key, value];
+  } else {
+    bucket.push([key, value]);
+  }
 };
-
-HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.retrieve = function(key) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  if (this._storage.get(index) === undefined) {
+    return undefined;
+  }
+  var bucket = this._storage.get(index);
+  for (var slot = 0; slot < bucket.length; slot++) {
+    if (bucket[slot][0] === key) {
+      return bucket[slot][1];
+    }
+  }
+  return undefined;
 };
-
-HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.remove = function(key) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  if (this._storage.get(index) === undefined) {
+    return -1;
+  }
+  var bucket = this._storage.get(index);
+  var existingSlot = -1;
+  for (var slot = 0; slot < bucket.length; slot++) {
+    if (bucket[slot][0] === key) {
+      existingSlot = slot;
+      break;
+    }
+  }
+  if (existingSlot > -1) {
+    bucket.splice(existingSlot, 1);
+  } else {
+    return -1;
+  }
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ *
+ * HashTable.prototype.insert: O(1)
+ * HashTable.prototype.retrieve: O(1)
+ * HashTable.prototype.remove: O(1)
  */
 
 
