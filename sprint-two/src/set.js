28b1c38fd396e173002b8636371740186d7a838f
Errol Cleary <errol.cleary@gmail.com>
2020-11-28 13:41:48 -0800

    Refactor Set to use HashTable

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,21 +1,39 @@
 var Set = function() {
   var set = Object.create(setMethods);
+  set.storage = new HashTable();
   return set;
 };
 
 var setMethods = {};
 
+// Original implementation
+
+// // Time complexity: O(1)
+// setMethods.add = function(value) {
+//   this[value] = value;
+// };
+// // Time complexity: O(1)
+// setMethods.contains = function(value) {
+//   return this[value] !== undefined;
+// };
+// // Time complexity: O(1)
+// setMethods.remove = function(value) {
+//   if (this[value] !== undefined) {
+//     delete this[value];
+//   }
+// };
+
+// Refactored implementation using custom hash table instead of built-in object data structure
+
 // Time complexity: O(1)
 setMethods.add = function(value) {
-  this[value] = value;
+  this.storage.insert(value, value);
 };
 // Time complexity: O(1)
 setMethods.contains = function(value) {
-  return this[value] !== undefined;
+  return this.storage.retrieve(value) !== undefined;
 };
 // Time complexity: O(1)
 setMethods.remove = function(value) {
-  if (this[value] !== undefined) {
-    delete this[value];
-  }
+  this.storage.remove(value);
 };
