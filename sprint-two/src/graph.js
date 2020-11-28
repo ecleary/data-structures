f894546e7cf0fb1177311c19823433773b8ef208
Errol Cleary <errol.cleary@gmail.com>
2020-11-27 16:41:37 -0800

    Implement Graph in pseudoclassical instantiation style

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,39 +1,54 @@
+var Graph = function() {};
 
-
-// Instantiate a new graph
-var Graph = function() {
+// Time complexity: O(1)
+Graph.prototype.addNode = function(value) {
+  if (this[value] === undefined) {
+    this[value] = [];
+  }
 };
-
-// Add a node to the graph, passing in the node's value.
-Graph.prototype.addNode = function(node) {
+// Time complexity: O(1)
+Graph.prototype.contains = function(value) {
+  return this[value] !== undefined ? true : false;
 };
-
-// Return a boolean value indicating if the value passed to contains is represented in the graph.
-Graph.prototype.contains = function(node) {
+// Time complexity: O(n) due to lookup-by-value in .removeEdge() call
+Graph.prototype.removeNode = function(targetNode) {
+  if (this[targetNode] !== undefined) {
+    var nodesNeedingEdgeRemoval = this[targetNode];
+    nodesNeedingEdgeRemoval.forEach.call(this, function(nodeNeedingEdgeRemoval) {
+      this.removeEdge(nodeNeedingEdgeRemoval, targetNode);
+    });
+    delete this[targetNode];
+  }
 };
-
-// Removes a node from the graph.
-Graph.prototype.removeNode = function(node) {
-};
-
-// Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
-Graph.prototype.hasEdge = function(fromNode, toNode) {
-};
-
-// Connects two nodes in a graph by adding an edge between them.
+// Time complexity: O(n) due to lookup-by-value
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  if (this[fromNode] === undefined || this[toNode] === undefined) {
+    return -1;
+  }
+  if (this[fromNode].indexOf(toNode) === -1) {
+    this[fromNode].push(toNode);
+    this[toNode].push(fromNode);
+  }
 };
-
-// Remove an edge between any two specified (by value) nodes.
+// Time complexity: O(n) due to lookup-by-value
+Graph.prototype.hasEdge = function(fromNode, toNode) {
+  if (this[fromNode] === undefined || this[toNode] === undefined) {
+    return false;
+  }
+  return this[fromNode].indexOf(toNode) > -1;
+};
+// Time complexity: O(n) due to lookup-by-value
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  if (this[fromNode] === undefined || this[toNode] === undefined) {
+    return -1;
+  }
+  if (this[fromNode].indexOf(toNode) > -1) {
+    this[fromNode].splice(this[fromNode].indexOf(toNode), 1);
+    this[toNode].splice(this[toNode].indexOf(fromNode), 1);
+  }
 };
-
-// Pass in a callback which will be executed on each node of the graph.
-Graph.prototype.forEachNode = function(cb) {
+// Time complexity: O(n)
+Graph.prototype.forEachNode = function(callback) {
+  var nodes = Object.keys(this);
+  nodes.forEach(callback);
 };
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
-
-
