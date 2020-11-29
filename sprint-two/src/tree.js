f10449a0c8bb9e4ed491a2c84183343befcb0794
Errol Cleary <errol.cleary@gmail.com>
2020-11-29 10:56:27 -0800

    Add parent property and removeFromParent method to Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,6 +1,7 @@
 var Tree = function(value) {
   var tree = {};
   tree.value = value;
+  tree.parent = null;
   tree.children = [];
   _.extend(tree, treeMethods);
   return tree;
@@ -11,8 +12,17 @@ var treeMethods = {};
 // Time complexity: O(1)
 treeMethods.addChild = function(value) {
   var newTree = Tree(value);
+  newTree.parent = this;
   this.children.push(newTree);
   return newTree;
+};
+treeMethods.removeFromParent = function() {
+  var childValues = this.parent.children.map(function(child) {
+    return child.value;
+  });
+  var childNumber = childValues.indexOf(this.value);
+  this.parent.children.splice(childNumber, 1);
+  this.parent = null;
 };
 // Time complexity: O(n)
 treeMethods.contains = function(value) {
